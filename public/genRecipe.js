6552f9cbb91e34de24ad3db9a133b844b1ce931b
Monica Holliday <monicaholliday@github.com>
2018-06-11 10:02:13 -0700

    Hooked home page to Generate Recipes page

--- public/genRecipe.js
+++ public/genRecipe.js
@@ -2,16 +2,16 @@
 
 
 //add Generated Recipes in from database
-function insertRecipe(recipeName, recipeTime, recipeImg) {
-
-  var recipeTemplate = Handlebars.templates.recipe;
-  var newRecipeHTML = recipeTemplate({
-    Name: recipeName,
-    time: recipeTime,
-    image: recipeImg
-  });
-
-  var recipeContainer = document.querySelector('main.recipe-container');
-
-  recipeContainer.insertAdjacentHTML('beforeend', newRecipeHTML)''
-}
+// function insertRecipe(recipeName, recipeTime, recipeImg) {
+//
+//   var recipeTemplate = Handlebars.templates.recipe;
+//   var newRecipeHTML = recipeTemplate({
+//     Name: recipeName,
+//     time: recipeTime,
+//     image: recipeImg
+//   });
+//
+//   var recipeContainer = document.querySelector('main.recipe-container');
+//
+//   recipeContainer.insertAdjacentHTML('beforeend', newRecipeHTML);
+// }
